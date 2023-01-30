bcfc9f47e2ceae380aa72209e4e05f030dc692a0
menna walid <mennawalidrahma@gmail.com>
2023-01-30 03:29:05 +0200

    made the cubit and state

--- lib/bussiness_logic/cubit/characters_cubit.dart
+++ lib/bussiness_logic/cubit/characters_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
+import 'package:rick_and_morty/data/repositories/characters_repository.dart';
+
+import '../../data/models/characters.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository charactersRepository;
+  late final List<Character> characters;
+  CharactersCubit(this.charactersRepository) : super(CharactersInitial());
+
+  List<Character> getAllCharacters() {
+    charactersRepository.getAllCharacters().then((characters) {
+      emit(CharactersLoaded(characters));
+      this.characters = characters;
+    });
+
+    return characters;
+  }
 }
--- lib/bussiness_logic/cubit/characters_state.dart
+++ lib/bussiness_logic/cubit/characters_state.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'characters_cubit.dart';
 
 @immutable
 abstract class CharactersState {}
 
 class CharactersInitial extends CharactersState {}
+
+class CharactersLoaded extends CharactersState {
+  final List<Character> characters;
+
+  CharactersLoaded(
+    this.characters,
+  );
+}
