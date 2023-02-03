e7286d9dec923504e23275bee6231400bb436fdb
menna walid <mennawalidrahma@gmail.com>
2023-02-03 07:48:48 +0200

    the url is sent from the ui

--- lib/data/repositories/characters_repository.dart
+++ lib/data/repositories/characters_repository.dart
@@ -2,18 +2,18 @@ import '../models/characters.dart';
 import '../web_services/characters_web_service.dart';
 
 class CharactersRepository {
- Future<List<Character>> getAllCharacters() async {
+ Future<List<Character>> getAllCharacters(String url) async {
     final characters =
-        await CharactersWebServices.getAllCharactersAndPagesInfo();
+        await CharactersWebServices.getAllCharactersAndPagesInfo(url);
     final data = PageInfoAndCharacters.fromJson(characters);
     return data.characters!;
   }
 
-  Future<Info?> getPageInfo() async {
+  Future<Info> getPageInfo(String url) async {
     final characters =
-        await CharactersWebServices.getAllCharactersAndPagesInfo();
+        await CharactersWebServices.getAllCharactersAndPagesInfo(url);
 
     final data = PageInfoAndCharacters.fromJson(characters);
-    return data.pagesInfo;
+    return data.pagesInfo!;
   }
 }
